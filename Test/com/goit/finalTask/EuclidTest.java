64d4d9176e0fbf1af83941df4e971f2863ff7a66
ded2055 <ded2055@yandex.ru>
2016-04-19 00:22:16 +0300

    add test

---
+++ Test/com/goit/finalTask/EuclidTest.java
@@ -0,0 +1,20 @@
+package com.goit.finalTask;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Ded on 18.04.2016.
+ */
+public class EuclidTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void greatestCommonDivisor() throws Exception {
+
+    }
+
+}
