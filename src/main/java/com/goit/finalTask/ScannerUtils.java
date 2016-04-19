fc537c2c72c02a0d123a53cdbd49fbd817ddb158
4594400 <4594400@gmail.com>
2016-04-19 17:33:59 +0300

    sixth commit Doc - Fix method "readOnlyIntegers" (added quit of the program)

--- src/main/java/com/goit/finalTask/ScannerUtils.java
+++ src/main/java/com/goit/finalTask/ScannerUtils.java
@@ -1,17 +1,19 @@
 package com.goit.finalTask;
 
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ScannerUtils {
     public static int readOnlyIntegers(Scanner in) {
         int integer;
         try {
-            integer = in.nextInt();
-        } catch (InputMismatchException e) {
-            System.err.println("Only integers are allowed. Try again! ->");
-            in.nextLine();
+            String input = in.nextLine();
+            if (input.equals("quit")) {
+                System.exit(0);
+            }
+            integer = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.err.println("Only integers are allowed. Try again or enter \"quit\" to exit the program! ->");
             integer = readOnlyIntegers(in);
         }
         return integer;
