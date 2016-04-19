cdfaf8bf813a0c98be6c9c3d0205925d55802bf7
4594400 <4594400@gmail.com>
2016-04-19 12:14:50 +0300

    third commit Doc - changing read variables

--- src/main/java/com/goit/finalTask/Main.java
+++ src/main/java/com/goit/finalTask/Main.java
@@ -2,6 +2,7 @@ package com.goit.finalTask;
 
 
 import java.io.PrintWriter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,13 +11,13 @@ public class Main {
         PrintWriter printWriter = new PrintWriter(System.out);
         Euclid euclid = new Euclid();
 
-        int a;
-        a = scanner.nextInt();
-        int b;
-        b = scanner.nextInt();
-
-        printWriter.println(euclid.greatestCommonDivisor(a, b));
-
+        try {
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        printWriter.println("Greatest common divisor of " + a + " and " + b + " is " + euclid.greatestCommonDivisor(a, b));
+        } catch (NumberFormatException e) {
+            System.out.println("|ERROR| The input values must be integers! Try again.");
+        }
         scanner.close();
         printWriter.close();
 
