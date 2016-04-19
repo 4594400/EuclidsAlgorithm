a32997125ef08d1babb04b9b8954fc71e87c5ea3
4594400 <4594400@gmail.com>
2016-04-20 01:13:41 +0300

    seventh commit Doc - Fix method "readOnlyIntegers" (added to the number of attempts to enter)

--- src/main/java/com/goit/finalTask/ScannerUtils.java
+++ src/main/java/com/goit/finalTask/ScannerUtils.java
@@ -4,18 +4,22 @@ package com.goit.finalTask;
 import java.util.Scanner;
 
 public class ScannerUtils {
+    public static int count = 3;
+
     public static int readOnlyIntegers(Scanner in) {
         int integer;
         try {
             String input = in.nextLine();
-            if (input.equals("quit")) {
+            if (input.equals("quit") | count==0) {
                 System.exit(0);
             }
             integer = Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.err.println("Only integers are allowed. Try again or enter \"quit\" to exit the program! ->");
+            System.err.println("Only integers are allowed. Enter \"quit\" to exit the program or try again! You have " + count + " attempts ->");
+            count--;
             integer = readOnlyIntegers(in);
         }
         return integer;
+
     }
 }
