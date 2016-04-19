001ab5fe56bf2ada9132d70c7987b558021074df
ded2055 <ded2055@yandex.ru>
2016-04-19 07:51:49 +0300

    create test for greatestCommonDivisor method.

--- Test/com/goit/finalTask/EuclidTest.java
+++ Test/com/goit/finalTask/EuclidTest.java
@@ -3,18 +3,26 @@ package com.goit.finalTask;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Created by Ded on 18.04.2016.
- */
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+
 public class EuclidTest {
+    private Euclid euclid;
     @Before
     public void setUp() throws Exception {
-
+         euclid = new Euclid();
     }
 
     @Test
-    public void greatestCommonDivisor() throws Exception {
-
+    public void testGreatestCommonDivisor() throws Exception {
+        for ( int x = -10000; x <= 10000; ++x )
+            for ( int y = -10000; y <= 10000; ++y )
+            {
+                int actual = Math.abs(euclid.greatestCommonDivisor(x, y));
+                int expected = BigInteger.valueOf(x).gcd(BigInteger.valueOf(y)).intValue();
+                assertEquals( expected, actual );
+            }
     }
 
 }
