3018bd686dbbed3eecbad1c3a049553f5c4dc7fd
ded2055 <ded2055@yandex.ru>
2016-04-20 21:27:27 +0300

    Merge branch 'Test' of https://github.com/4594400/EuclidsAlgorithm into HEAD
    # Conflicts:
    #	pom.xml

--- src/test/java/com/goit/finalTask/ScannerUtilsTest.java
+++ src/test/java/com/goit/finalTask/ScannerUtilsTest.java
@@ -1,5 +1,6 @@
 package com.goit.finalTask;
 
+import com.goit.finalTask.myExceptions.ExceedingTheNumberOfAllowedAttemptsToEnterException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,18 +19,32 @@ public class ScannerUtilsTest {
         scannerUtils = new ScannerUtils();
 
     }
+
     @Test
     public void testReadOnlyIntegersInputIntegerInStringFormat() throws Exception {
         String input = "600";
         Scanner in = new Scanner(input);
         int actual = scannerUtils.readOnlyIntegers(in);
         assertEquals(Integer.parseInt(input), actual);
-
     }
+
     @Test (expected = NoSuchElementException.class)
     public void testReadOnlyIntegersInputStringFormat() throws Exception {
         String input = "text";
         Scanner in = new Scanner(input);
         scannerUtils.readOnlyIntegers(in);
     }
+
+    @Test
+    public void testReadOnlyIntegersExceedingTheNumberOfAllowedAttemptsToEnterException() throws Exception{
+        Scanner in;
+        String input = "text";
+        for (int i = 0; i < 4; i++){
+            input = input + i;
+            in = new Scanner(input);
+            scannerUtils.readOnlyIntegers(in);
+        }
+
+    }
+
 }
